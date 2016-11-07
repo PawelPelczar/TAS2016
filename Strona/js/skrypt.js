fdaa28ce727b9b50be3fa0f742729772aafe0f82
5theLout <5theLout@gmail.com>
2016-11-07 16:24:07 +0100

    Add files via upload

--- Strona/js/skrypt.js
+++ Strona/js/skrypt.js
@@ -1,17 +1,19 @@
 
-var screenHeight=0;
-var screenWidth=0;
+var screenHeight;
+var screenWidth;
 function getScreenProperties(){
 	screenHeight=screen.height;
 	screenWidth=screen.width;
+	document.getElementById("ankieta_container").innerHTML = screen.width;
+	handleUpdate();
 }
 
-const inputs = [].slice.call(document.querySelectorAll('.controls input'));
+//const inputs = [].slice.call(document.querySelectorAll('.controls input'));
 
 // listen for changes
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+//inputs.forEach(input => input.addEventListener('change', handleUpdate));
+//inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
-function handleUpdate(e) {
-    document.documentElement.style.setProperty('screenHeight', this.value);
+function handleUpdate(val) {
+    document.documentElement.style.setProperty('screenHeight', val);
 }
